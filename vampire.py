90b176fe6950db13c953589ba1231c65901b496a
Trav <tjbailey10@gmail.com>
2023-07-12 15:48:05 -0400

    initial commit

---
+++ vampire.py
@@ -0,0 +1,10 @@
+name = 'Tam'
+age = 420
+if name == 'Tam':
+    print('Hi Tam')
+elif age < 12:
+    print('You are not Tam, kiddo.')
+elif age > 2000:
+    print('Unlike you, Tam is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Tam, grannie.')
